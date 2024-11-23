d63e31cc690c20722fa78d0b473560241bc45601
Azad Baykara <azad.baykara@tum.de>
2024-11-23 17:24:58 +0100

    bash script update

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# https://www.kaggle.com/datasets/taweilo/loan-approval-classification-data
 wget -O loan-approval.zip https://www.kaggle.com/api/v1/datasets/download/taweilo/loan-approval-classification-data
 unzip -o loan-approval.zip
 rm loan-approval.zip
